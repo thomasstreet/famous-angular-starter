95dc583230556125ec9b6897896dfc0394556605
Zack Brown <z@charybrown.com>
2014-05-20 20:06:01 -0700

    add transition

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -3,6 +3,7 @@
 angular.module('famousAngularStarter')
   .controller('MainCtrl', function ($scope, $famous) {
     var Transitionable = $famous['famous/transitions/Transitionable'];
+    var Easing = $famous['famous/transitions/Easing'];
 
     $scope.greeting = "Hello, Famo.us";
 
@@ -11,12 +12,18 @@ angular.module('famousAngularStarter')
     $scope.widthTransitionable = new Transitionable(1);
     $scope.heightTransitionable = new Transitionable(1);
 
+    //let's define our animation's transition
+    var transition = {
+      duration: 1000, //ms
+      curve: Easing.outBounce
+    }
+
     $scope.clickCenterBox = function(){
       //let's make that background box grow
       var xScale = window.innerWidth / $scope.BG_SIZE[0];
       var yScale = window.innerHeight / $scope.BG_SIZE[1];
-      $scope.widthTransitionable.set(xScale);
-      $scope.heightTransitionable.set(yScale);
+      $scope.widthTransitionable.set(xScale, transition);
+      $scope.heightTransitionable.set(yScale, transition);
     }
 
 
