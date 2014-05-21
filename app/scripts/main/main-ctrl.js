8747d7cf74b39315128d3a991b331861935e090e
Zack Brown <z@charybrown.com>
2014-05-20 20:51:18 -0700

    add reset toggle

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -24,12 +24,22 @@ angular.module('famousAngularStarter')
       curve: 'easeOut'
     }
 
+    var _state = false;
+
     $scope.clickCenterBox = function(){
-      //let's make that background box grow
-      var xScale = window.innerWidth / $scope.BG_SIZE[0];
-      var yScale = window.innerHeight / $scope.BG_SIZE[1];
-      $scope.widthTransitionable.set(xScale, transition);
-      $scope.heightTransitionable.set(yScale, transition, $scope.kickOffOpacityTransition);
+
+      if(!_state){
+        //let's make that background box grow
+        var xScale = window.innerWidth / $scope.BG_SIZE[0];
+        var yScale = window.innerHeight / $scope.BG_SIZE[1];
+        $scope.widthTransitionable.set(xScale, transition);
+        $scope.heightTransitionable.set(yScale, transition, $scope.kickOffOpacityTransition);
+      }else{
+        $scope.widthTransitionable.set(1, opacityTransition);
+        $scope.heightTransitionable.set(1, opacityTransition);
+        $scope.opacityTransitionable.set(1, opacityTransition);
+      }
+      _state = !_state;
     }
 
     $scope.kickOffOpacityTransition = function(){
