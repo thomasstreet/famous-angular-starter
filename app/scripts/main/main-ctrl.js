a35bfc55d42af4b6442752f0c7358c065481be79
Zack Brown <z@charybrown.com>
2014-06-20 12:19:56 -0700

    add simple spinning logo demo

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('famousAngularStarter')
-  .controller('MainCtrl', function ($scope) {
-    $scope.greeting = 'Hello, Famo.us';
+  .controller('MainCtrl', function ($scope, $famous) {
+    var Transitionable = $famous['famous/transitions/Transitionable'];
+    var Timer = $famous['famous/utilities/Timer'];
+
+    $scope.spinner = {
+      speed: 55
+    };
+    $scope.rotateY = new Transitionable(0);
+
+    //run function on every tick of the Famo.us engine
+    Timer.every(function(){
+      var adjustedSpeed = parseFloat($scope.spinner.speed) / 1200;
+      $scope.rotateY.set($scope.rotateY.get() + adjustedSpeed);
+    }, 1);
+
   });
